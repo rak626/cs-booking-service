c3cbd14b04350c68dbc2463f6cf3e36c8c0c284e
Rakesh Ghosh <rakeshacot@gmail.com>
2024-12-01 03:26:49 +0530

    initial commit

---
+++ src/main/java/com/crowdseats/cs_booking_service/CsBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crowdseats.cs_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crowdseats/cs_booking_service/CsBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdseats.cs_booking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
